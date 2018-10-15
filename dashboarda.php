a200ffbda9616fa12fc352d080ef125419885e03
qwerty999999999 <44159674+qwerty999999999@users.noreply.github.com>
2018-10-15 19:13:55 +0530

    Update dashboarda.php

--- dashboarda.php
+++ dashboarda.php
@@ -3,11 +3,7 @@ session_start();
 if(!isset($_SESSION['usernamea'])){
     header("Refresh:0; url=indexd.php");
 }
-sleep();
-$servername = "localhost";
-$username = "";
-$password = "";
-$conn = new mysqli($servername, $username, $password,'');
+require 'connect.php';
 $i=0;
 $s="SELECT * FROM attendance";
     $q=mysqli_query($conn,$s);
