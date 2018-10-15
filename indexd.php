87114cc43d851637d2c26a6879c84163a2a7ecab
qwerty999999999 <44159674+qwerty999999999@users.noreply.github.com>
2018-10-15 19:15:00 +0530

    Update indexd.php

--- indexd.php
+++ indexd.php
@@ -7,10 +7,7 @@ if(isset($_SESSION['username'])){
 if(isset($_SESSION['usernamea'])){
     header("Refresh:0; url=admin.php");
 }
-$servername = "localhost";
-$username = "";
-$password = "";
-$conn = new mysqli($servername, $username, $password,'');
+require 'connect.php';
 if(isset($_POST['submit'])){
 	$u=$_POST['username'];
 	
