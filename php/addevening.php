54fd998b69faaa5718ff50d203771489c11e2bf6
qwerty999999999 <44159674+qwerty999999999@users.noreply.github.com>
2018-10-15 19:33:40 +0530

    Update addevening.php

--- php/addevening.php
+++ php/addevening.php
@@ -25,7 +25,7 @@ header("Refresh:0; url=dashboardevening.php");
   <meta charset="UTF-8">
   <title>Add a new User</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="css/styleq.css">
+<link rel="stylesheet" type="text/css" href="../css/styleq.css">
 </head>
 
 <body>
